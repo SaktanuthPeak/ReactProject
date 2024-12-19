e2ac8c8bfd846cb95776298c052cdb7b5eb67448
SaktanuthPeak <thanakrit.5122548@gmail.com>
2024-12-19 13:09:50 +0700

    add remember me feature

--- src/screen/LoginScreen.js
+++ src/screen/LoginScreen.js
@@ -1,11 +1,25 @@
-import { useState } from 'react';
-import { Button, Form, Input, Alert } from 'antd';
+import { useEffect, useState } from 'react';
+import { Button, Form, Input, Alert, Checkbox } from 'antd';
 import axios from 'axios'
 const URL_AUTH = "/api/auth/local"
 
 export default function LoginScreen(props) {
     const [isLoading, setIsLoading] = useState(false)
     const [errMsg, setErrMsg] = useState(null)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [rememberMe, setRememberMe] = useState(false)
+
+    useEffect(() => {
+        const savedUsername = localStorage.getItem('username');
+        const savedPassword = localStorage.getItem('password');
+        if (savedUsername && savedPassword) {
+            setUsername(savedUsername);
+            setPassword(savedPassword);
+            setRememberMe(true);
+
+        }
+    })
     const handleLogin = async (formData) => {
         try {
             setIsLoading(true)
@@ -13,6 +27,18 @@ export default function LoginScreen(props) {
             const response = await axios.post(URL_AUTH, { ...formData })
             const token = response.data.jwt
             axios.defaults.headers.common = { 'Authorization': `bearer ${token}` }
+
+
+            if (rememberMe) {
+                localStorage.setItem('username', formData.identifier);
+                localStorage.setItem('password', formData.password);
+
+            } else {
+                localStorage.removeItem('username')
+                localStorage.removeItem('password')
+
+            }
+
             props.onLoginSuccess();
         } catch (err) {
             console.log(err)
@@ -33,15 +59,26 @@ export default function LoginScreen(props) {
                     <Form.Item
                         label="Username"
                         name="identifier"
+                        onChange={(e) => setUsername(e.target.values)}
                         rules={[{ required: true, }]}>
                         <Input />
                     </Form.Item>
                     <Form.Item
                         label="Password"
                         name="password"
+                        onChange={(e) => setPassword(e.target.values)}
                         rules={[{ required: true },]}>
                         <Input.Password />
                     </Form.Item>
+                    <Form.Item>
+                        <Checkbox
+                            checked={rememberMe}
+                            onChange={(e) => setRememberMe(e.target.checked)}
+                        >
+                            Remember Me
+                        </Checkbox>
+                    </Form.Item>
+
                     <Form.Item>
                         <Button
                             type="primary"
